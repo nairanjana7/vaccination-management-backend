67c8512671293fc999a5ee8d3400973da0afb1ba
nairanjana7 <nairanjana7904@gmail.com>
2026-02-18 16:43:09 +0530

    Fix auth route callback issue and enable demo login flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  registerUser,
-  loginUser,
-} = require("../controllers/authController");
+const login = require("../controllers/authController");
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login", login);
 
 module.exports = router;
